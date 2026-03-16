f03130e1f1d979fcee3b69b4524094a9257d2a4e
Devansh Srivastava <devanshsri555@gmail.com>
2026-03-17 00:09:11 +0530

    Add application entry and welcome message

---
+++ UC1: Application Entry & Welcome Message.java
@@ -0,0 +1,17 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("       PALINDROME CHECKER APP     ");
+        System.out.println("==================================");
+
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Application Version : 1.0");
+
+        System.out.println("----------------------------------");
+        System.out.println("Application Started Successfully!");
+        System.out.println("----------------------------------");
+
+    }
+}
