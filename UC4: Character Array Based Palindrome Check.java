ca6d5f5dbb29bb51531724c73aaac2ceae82b045
Devansh Srivastava <devanshsri555@gmail.com>
2026-03-17 00:12:48 +0530

    Add palindrome checker using character array

---
+++ UC4: Character Array Based Palindrome Check.java
@@ -0,0 +1,28 @@
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "level";
+        char[] characters = word.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+
+        while(start < end) {
+            if(characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if(isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+
+    }
+}
