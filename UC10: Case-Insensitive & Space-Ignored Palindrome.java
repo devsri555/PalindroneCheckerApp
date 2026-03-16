de9a12aeb69ed025ca948ad5d588f0f97849b334
Devansh Srivastava <devanshsri555@gmail.com>
2026-03-17 00:21:59 +0530

    Add palindrome checker for case-insensitive input

---
+++ UC10: Case-Insensitive & Space-Ignored Palindrome.java
@@ -0,0 +1,29 @@
+public class UseCase10PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "Madam In Eden Im Adam";
+
+        // Normalize string: remove spaces and convert to lowercase
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = normalized.length() - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("\"" + input + "\" is a Palindrome");
+        } else {
+            System.out.println("\"" + input + "\" is NOT a Palindrome");
+        }
+    }
+}
