8a9fb3c928bde4156bbd7d7c1089b5bccca40fa2
Devansh Srivastava <devanshsri555@gmail.com>
2026-03-17 00:23:16 +0530

    Implement palindrome checker and application

---
+++ UC11: Object-Oriented Palindrome Service.java
@@ -0,0 +1,32 @@
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String word) {
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        PalindromeChecker checker = new PalindromeChecker();
+
+        if (checker.checkPalindrome(word)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
